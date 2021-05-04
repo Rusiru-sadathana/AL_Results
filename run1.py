9eb5b5580b4555c555fd284da2b87d30cc9bc0a5
Rusiru-sadathana <rusirusadathana@gmail.com>
2021-05-04 22:44:51 +0530

    Update run1.py

--- run1.py
+++ run1.py
@@ -1,13 +1,8 @@
 import requests
 import json
 import csv
-from requests.structures import CaseInsensitiveDict
 
-
-token='1776016878:AAEWXP7zSt9abR7i-Ev_Y9L_fSd1U2nR-Ko'
-
-
-def get_data(index_no,g):
+def get_data(index_no):
     try:
         r=requests.get('https://www.doenets.lk/result/service/AlResult/'+str(index_no))
         jsondata = json.loads(r.text)
@@ -37,11 +32,8 @@ def get_data(index_no,g):
             arr=[year,name,indexNo,nic,districtRank,islandRank,zScore,stream,Sub1name,Sub1result,Sub2name,Sub2result,Sub3name,Sub3result]
             print(indexNo)
             
-            msg=year+'\n'+name+'\n'+indexNo+'\n'+nic+'\n'+districtRank+'\n'+islandRank+'\n'+zScore+'\n'+stream+'\n'+Sub1name+' '+Sub1result+'\n'+Sub2name+' '+Sub2result+'\n'+Sub3name+' '+Sub3result
-            method='sendMessage'
-            #response = requests.post(url='https://api.telegram.org/bot{0}/{1}'.format(token, method), data={'chat_id':-1001380999473, 'text': msg}).json() 
-
-            with open('results_1.csv', 'a') as csvFile:
+            
+            with open('results.csv', 'a') as csvFile:
                 writer = csv.writer(csvFile)
                 writer.writerow(arr)
                 csvFile.close()
@@ -49,10 +41,8 @@ def get_data(index_no,g):
     except:
         print('err')
 
-fromm=5000000  
-to=7499999
-g=1
-for i in range(fromm,to):
-	get_data(i,g)
+fromm=1800000  
+to=2500000
 
-#response = requests.post(url='https://api.telegram.org/bot{0}/{1}'.format(token, method),data={'chat_id':-1001380999473, 'text': 'Done start again'}).json()
+for i in range(fromm,to):
+  get_data(i)
